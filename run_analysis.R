46c309a866f90614fe8ebfd19ade5f1d9df70bab
Chas Threlkeld <chathrel@indiana.edu>
2017-08-03 10:21:13 -0500

    script complete

--- run_analysis.R
+++ run_analysis.R
@@ -21,6 +21,17 @@ if(!dir.exists("UCI HAR Dataset")) {
 #' dplyr is required for data frame management
 library(dplyr)
 
+# Declare the measurments in which we are interested. All other data in the 
+# dataset are futher derived from these.
+measurements <- c("acceleration.x.mean","acceleration.y.mean",
+                  "acceleration.z.mean",
+                  "acceleration.x.std","acceleration.y.std",
+                  "acceleration.z.std",
+                  "gravity.x.mean","gravity.y.mean","gravity.z.mean",
+                  "gravity.x.std","gravity.y.std","gravity.z.std",
+                  "gyro.x.mean","gyro.y.mean","gyro.z.mean",
+                  "gyro.x.std","gyro.y.std","gyro.z.std")
+
 #' get_set does the heavy lifting of bringing in the data and cleaning it up.
 #' 
 #' Using the given names: x is the dataset of derived data from the raw data 
@@ -37,43 +48,57 @@ library(dplyr)
 #' the y_file to the character labels given in activity_labels.txt.
 get_set <- function(test_or_train) {
     
+    # Get the x.txt file
     x_file <- paste("X_", test_or_train, ".txt", sep="")
     x <- read.table(file.path("UCI HAR Dataset", test_or_train, x_file))
     
+    # Label x according to the features file
     features <- read.table(file.path("UCI HAR Dataset","features.txt"))
     names(x) <- make.names(features$V2)
     
+    # Remove duplicated columns
     x <- x[, !duplicated(colnames(x))]
     
+    # Get only the mean and standard deviation columns for the measured data
     regex <- "^t(Body|Gravity)(Gyro|Acc).(mean|std)...[XYZ]"
     x <- select(x, matches(regex))
     
-    names(x) <- c("acceleration.x.mean","acceleration.y.mean","acceleration.z.mean",
-                  "acceleration.x.std","acceleration.y.std","acceleration.z.std",
-                  "gravity.x.mean","gravity.y.mean","gravity.z.mean",
-                  "gravity.x.std","gravity.y.std","gravity.z.std",
-                  "gyro.x.mean","gyro.y.mean","gyro.z.mean",
-                  "gyro.x.std","gyro.y.std","gyro.z.std")
+    # Rename the columns following R conventions
+    names(x) <- measurements
     
+    # Add a column to label the subjects performing the tests
     subject_file <- paste("subject_", test_or_train, ".txt", sep="")
-    subjects <- read.table(file.path("UCI HAR Dataset", test_or_train, subject_file))
+    subjects <- read.table(file.path("UCI HAR Dataset", test_or_train, 
+                                     subject_file))
     x <- mutate(x, subject = subjects$V1)
     
-    activity_labels <- read.table(file.path("UCI HAR Dataset", "activity_labels.txt"))
+    # Add a column to label the activity performed for each row
+    activity_labels <- read.table(file.path("UCI HAR Dataset", 
+                                            "activity_labels.txt"))
     y_file <- paste("y_", test_or_train, ".txt", sep="")
     y <- read.table(file.path("UCI HAR Dataset", test_or_train, y_file))
     activities <- sapply(y, function(y_row){activity_labels[y_row,2]})
-    mutate(x, activity = activities)
+    mutate(x, activity = as.vector(activities))
 }
 
+# Retrieve both the test and train sets
 test <- get_set("test")
 train <- get_set("train")
 
-#' After processing the test and train data independently, they are merged into
-#' a single data set. Extra variables are removed from the environment so that
-#' the script cleans up after itself.
+# Merge the train and test sets to get all sets
 tidy_data <- merge(test, train, all=TRUE)
 
+# Create a second data set that is the average of each variable for each
+# activity and each subject.
+grouped_data <- group_by(tbl_df(tidy_data), subject, activity)
+measurement_averages <- aggregate(grouped_data[,1:18], 
+                                  list(grouped_data$subject, 
+                                       grouped_data$activity), mean)
+names(measurement_averages) <- c("subject", "activity", measurements)
+
+# Clean up after the script is done
+remove(grouped_data)
+remove(measurements)
 remove(test)
 remove(train)
 remove(get_set)
